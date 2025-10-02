87eec8a39f4702afddef1bfc24b1e60f0ea5722c
Stephon Treadwell <treadtech33@gmail.com>
2025-10-01 21:35:48 -0400

    modified variables

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "public_key" {
+  type        = string
+  description = "User public key"
+  default     = "./aws_keys.pub"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "aws_ami" {
+  type    = string
+  default = "ami-0953476d60561c955"
+}
+
+variable "aws_ssh_port" {
+  type    = number
+  default = 22
+}
+
+variable "aws_http_port" {
+  type    = number
+  default = 80
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS default region"
+}
+
+
+
